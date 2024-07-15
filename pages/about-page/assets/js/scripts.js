e133b0ee18bc4a57d1f071e265867f226fcac0e0
Kyla Mae Andrade <202311015@fit.edu.ph>
2024-07-15 22:21:29 +0800

    feat(about-page): created about me page

---
+++ pages/about-page/assets/js/scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.accordion-item button');
+
+    function toggleAccordion() {
+        const itemToggle = this.getAttribute('aria-expanded');
+
+        for (let i = 0; i < items.length; i++) {
+            items[i].setAttribute('aria-expanded', 'false');
+        }
+
+        if (itemToggle === 'false') {
+            this.setAttribute('aria-expanded', 'true');
+        }
+    }
+
+    items.forEach(item => item.addEventListener('click', toggleAccordion));
+});
